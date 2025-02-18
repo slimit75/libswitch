450df90169292c0c201c9f8df4dfe857f8a93a7e
Ian <ian@slimit75.me>
2025-02-17 20:53:36 -0500

    feat!: support multiple switch types

--- libswitch.c
+++ libswitch.c
@@ -6,83 +6,380 @@
 
 #define SWITCH_GAIN 0.2f
 
-switch_t *all_sw;
+struct {
+	int state;
+	float act_gain;
+	float anim_pos;
+	XPLMDataRef dr_state;
+	XPLMDataRef dr_anim;
+	XPLMCommandRef cmd_toggle;
+} typedef sw_basic_t;
 
-int sw_button_cb(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon) {
+struct {
+	int min;
+	int max;
+	int state;
+	float act_gain;
+	float anim_pos;
+	bool starter;
+	XPLMDataRef dr_state;
+	XPLMDataRef dr_anim;
+	XPLMCommandRef cmd_toggle_l;
+	XPLMCommandRef cmd_toggle_r;
+} typedef sw_multi_t;
+
+sw_basic_t *all_basic_sw;
+sw_basic_t *all_spring_sw;
+sw_multi_t *all_multi_sw;
+
+int sw_basic_cb(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon) {
 	int i = (int)inRefcon;
 
-	if (all_sw[i].state == 0) {
-		all_sw[i].act_gain = SWITCH_GAIN;
+	if (all_basic_sw[i].state == 0) {
+		all_basic_sw[i].act_gain = SWITCH_GAIN;
 	}
 	else {
-		all_sw[i].act_gain = -SWITCH_GAIN;
+		all_basic_sw[i].act_gain = -SWITCH_GAIN;
 	}
 
-	all_sw[i].state = !all_sw[i].state;
+	all_basic_sw[i].state = !all_basic_sw[i].state;
 
 	return 1;
 }
 
-void sw_ref() {
-	for (int i = 0; i < (sizeof(&all_sw) / sizeof(all_sw[0])); i++) {
-		if (all_sw[i].act_gain != 0) {
-			all_sw[i].anim_pos += all_sw[i].act_gain;
+int sw_spring_cb(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon) {
+	int i = (int)inRefcon;
+
+	if (inPhase == 0) {
+		all_spring_sw[i].act_gain = SWITCH_GAIN;
+		all_spring_sw[i].state = 1;
+	}
+	else if (inPhase == 2) {
+		all_spring_sw[i].act_gain = -SWITCH_GAIN;
+		all_spring_sw[i].state = 0;
+	}
+	return 1;
+}
+
+int sw_multi_l_cb(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon) {
+	int i = (int)inRefcon;
+
+	if ((inPhase == 0) && (all_multi_sw[i].state > all_multi_sw[i].min)) {
+		all_multi_sw[i].act_gain = -SWITCH_GAIN;
+		all_multi_sw[i].state -= 1;
+	}
+
+	return 1;
+}
+
+int sw_multi_r_cb(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon) {
+	int i = (int)inRefcon;
+
+	if ((inPhase == 0) && (all_multi_sw[i].state < all_multi_sw[i].max)) {
+		all_multi_sw[i].act_gain = SWITCH_GAIN;
+		all_multi_sw[i].state += 1;
+	}
+	else if ((all_multi_sw[i].starter == true) && (all_multi_sw[i].state == all_multi_sw[i].max) && (inPhase == 2)) {
+		all_multi_sw[i].act_gain = -SWITCH_GAIN;
+		all_multi_sw[i].state -= 1;
+	}
+
+	return 1;
+}
+
+// TODO: can this refresh section be better?
+void sw_ref_basic(sw_basic_t *array) {
+	for (int i = 0; i < (sizeof(&array) / sizeof(array[0])); i++) {
+		if (array[i].act_gain != 0) {
+			array[i].anim_pos += array[i].act_gain;
 
 			// TODO: holy if statement
-			if (((all_sw[i].act_gain < 0) && (all_sw[i].anim_pos < (float)all_sw[i].state)) || (
-					(all_sw[i].act_gain > 0) && (all_sw[i].anim_pos > (float)all_sw[i].state))) {
-				all_sw[i].anim_pos = (float)all_sw[i].state;
-				all_sw[i].act_gain = 0;
+			if (((array[i].act_gain < 0) && (array[i].anim_pos < (float)array[i].state)) || (
+					(array[i].act_gain > 0) && (array[i].anim_pos > (float)array[i].state))) {
+				array[i].anim_pos = (float)array[i].state;
+				array[i].act_gain = 0;
 			}
 		}
-		else if (all_sw[i].anim_pos != (float)all_sw[i].state) {
-			all_sw[i].anim_pos = (float)all_sw[i].state;
+		else if (array[i].anim_pos != (float)array[i].state) {
+			array[i].anim_pos = (float)array[i].state;
 		}
 	}
 }
 
-int sw_dr_fetch_state(void *inRefcon) {
-	return all_sw[(int)inRefcon].state;
+void sw_ref() {
+	sw_ref_basic(all_basic_sw);
+	sw_ref_basic(all_spring_sw);
+
+	for (int i = 0; i < (sizeof(&all_multi_sw) / sizeof(all_multi_sw[0])); i++) {
+		if (all_multi_sw[i].act_gain != 0) {
+			all_multi_sw[i].anim_pos += all_multi_sw[i].act_gain;
+
+			// TODO: holy if statement
+			if (((all_multi_sw[i].act_gain < 0) && (all_multi_sw[i].anim_pos < (float)all_multi_sw[i].state)) || (
+					(all_multi_sw[i].act_gain > 0) && (all_multi_sw[i].anim_pos > (float)all_multi_sw[i].state))) {
+				all_multi_sw[i].anim_pos = (float)all_multi_sw[i].state;
+				all_multi_sw[i].act_gain = 0;
+			}
+		}
+		else if (all_multi_sw[i].anim_pos != (float)all_multi_sw[i].state) {
+			all_multi_sw[i].anim_pos = (float)all_multi_sw[i].state;
+		}
+	}
 }
 
-void sw_dr_write_state(void *inRefcon, int inValue) {
-	all_sw[(int)inRefcon].state = inValue;
+int sw_basic_get_state(void *inRefcon) {
+	return all_basic_sw[(int)inRefcon].state;
 }
 
-float sw_dr_fetch_anim(void *inRefcon) {
-	return all_sw[(int)inRefcon].anim_pos;
+void sw_basic_write_state(void *inRefcon, int inValue) {
+	all_basic_sw[(int)inRefcon].state = inValue;
 }
 
-switch_t *sw_basic_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc) {
+float sw_basic_get_anim(void *inRefcon) {
+	return all_basic_sw[(int)inRefcon].anim_pos;
+}
+
+int sw_spring_get_state(void *inRefcon) {
+	return all_spring_sw[(int)inRefcon].state;
+}
+
+void sw_spring_write_state(void *inRefcon, int inValue) {
+	all_spring_sw[(int)inRefcon].state = inValue;
+}
+
+float sw_spring_get_anim(void *inRefcon) {
+	return all_spring_sw[(int)inRefcon].anim_pos;
+}
+
+int sw_multi_get_state(void *inRefcon) {
+	return all_multi_sw[(int)inRefcon].state;
+}
+
+void sw_multi_write_state(void *inRefcon, int inValue) {
+	all_multi_sw[(int)inRefcon].state = inValue;
+}
+
+float sw_multi_get_anim(void *inRefcon) {
+	return all_multi_sw[(int)inRefcon].anim_pos;
+}
+
+switch_t sw_basic_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc) {
 	int idx;
-	if (!all_sw) {
-		all_sw = malloc(sizeof(switch_t));
-		idx    = 0;
+	if (all_basic_sw == NULL) {
+		all_basic_sw = malloc(sizeof(sw_basic_t));
+		idx = 0;
 	}
 	else {
-		idx = (sizeof(&all_sw) / sizeof(all_sw[0]));
+		all_basic_sw = realloc(all_basic_sw, sizeof(all_basic_sw) + sizeof(sw_basic_t));
+		idx = (sizeof(&all_basic_sw) / sizeof(sw_basic_t));
 	}
 
-	all_sw[idx].state    = 0;
-	all_sw[idx].anim_pos = 0;
-	all_sw[idx].act_gain = 0;
+	all_basic_sw[idx].state = 0;
+	all_basic_sw[idx].anim_pos = 0;
+	all_basic_sw[idx].act_gain = 0;
 
-	XPLMCreateCommand(cmd_name, cmd_desc);
-	XPLMRegisterCommandHandler(all_sw[idx].cmd_toggle, sw_button_cb, 1, (void *)idx);
-	XPLMRegisterDataAccessor(dr_name, xplmType_Int, true, sw_dr_fetch_state, sw_dr_write_state, NULL, NULL, NULL, NULL,
-							 NULL, NULL,
-							 NULL, NULL, NULL, NULL, (void *)idx, (void *)idx);
-	XPLMRegisterDataAccessor(dr_anim_name, xplmType_Float, false, NULL, NULL, sw_dr_fetch_anim, NULL, NULL, NULL, NULL,
-							 NULL,
-							 NULL, NULL, NULL, NULL, (void *)idx, NULL);
+	all_basic_sw[idx].cmd_toggle = XPLMCreateCommand(cmd_name, cmd_desc);
+	XPLMRegisterCommandHandler(all_basic_sw[idx].cmd_toggle, sw_basic_cb, 1, (void *)idx);
 
-	return &all_sw[idx];
-};
-
-void sw_destroy_all() {
-	for (int i = 0; i < (sizeof(&all_sw) / sizeof(all_sw[0])); i++) {
-		XPLMUnregisterCommandHandler(all_sw[i].cmd_toggle, sw_button_cb, 1, (void *)i);
+	if (dr_name) {
+		XPLMRegisterDataAccessor(
+			dr_name,
+			xplmType_Int,
+			true,
+			sw_basic_get_state,
+			sw_basic_write_state,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			(void *)idx
+		);
 	}
 
-	free(all_sw);
+	if (dr_anim_name) {
+		XPLMRegisterDataAccessor(
+			dr_anim_name,
+			xplmType_Float,
+			false,
+			NULL,
+			NULL,
+			sw_basic_get_anim,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			NULL
+		);
+	}
+
+	return idx;
+}
+
+switch_t sw_spring_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc) {
+	int idx;
+	if (all_spring_sw == NULL) {
+		all_spring_sw = malloc(sizeof(sw_basic_t));
+		idx = 0;
+	}
+	else {
+		all_spring_sw = realloc(all_spring_sw, sizeof(all_spring_sw) + sizeof(sw_basic_t));
+		idx = (sizeof(&all_spring_sw) / sizeof(sw_basic_t));
+	}
+
+	all_spring_sw[idx].state = 0;
+	all_spring_sw[idx].anim_pos = 0;
+	all_spring_sw[idx].act_gain = 0;
+
+	all_spring_sw[idx].cmd_toggle = XPLMCreateCommand(cmd_name, cmd_desc);
+	XPLMRegisterCommandHandler(all_spring_sw[idx].cmd_toggle, sw_spring_cb, 1, (void *)idx);
+
+	if (dr_name) {
+		XPLMRegisterDataAccessor(
+			dr_name,
+			xplmType_Int,
+			true,
+			sw_spring_get_state,
+			sw_spring_write_state,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			(void *)idx
+		);
+	}
+
+	if (dr_anim_name) {
+		XPLMRegisterDataAccessor(
+			dr_anim_name,
+			xplmType_Float,
+			false,
+			NULL,
+			NULL,
+			sw_spring_get_anim,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			NULL
+		);
+	}
+
+	return idx;
+}
+
+switch_t sw_multi_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name_l, const char *cmd_desc_l,
+					   const char *cmd_name_r, const char *cmd_desc_r, int min_range, int max_range, int default_value,
+					   bool starter) {
+	int idx;
+	if (all_multi_sw == NULL) {
+		all_multi_sw = malloc(sizeof(sw_multi_t));
+		idx = 0;
+	}
+	else {
+		all_multi_sw = realloc(all_multi_sw, sizeof(all_multi_sw) + sizeof(sw_multi_t));
+		idx = (sizeof(&all_multi_sw) / sizeof(sw_multi_t));
+	}
+
+	all_multi_sw[idx].state = default_value;
+	all_multi_sw[idx].anim_pos = 0;
+	all_multi_sw[idx].act_gain = 0;
+	all_multi_sw[idx].min = min_range;
+	all_multi_sw[idx].max = max_range;
+	all_multi_sw[idx].starter = starter;
+
+	all_multi_sw[idx].cmd_toggle_l = XPLMCreateCommand(cmd_name_l, cmd_desc_l);
+	all_multi_sw[idx].cmd_toggle_r = XPLMCreateCommand(cmd_name_r, cmd_desc_r);
+	XPLMRegisterCommandHandler(all_multi_sw[idx].cmd_toggle_l, sw_multi_l_cb, 1, (void *)idx);
+	XPLMRegisterCommandHandler(all_multi_sw[idx].cmd_toggle_r, sw_multi_r_cb, 1, (void *)idx);
+
+	if (dr_name) {
+		XPLMRegisterDataAccessor(
+			dr_name,
+			xplmType_Int,
+			true,
+			sw_multi_get_state,
+			sw_multi_write_state,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			(void *)idx
+		);
+	}
+
+	if (dr_anim_name) {
+		XPLMRegisterDataAccessor(
+			dr_anim_name,
+			xplmType_Float,
+			false,
+			NULL,
+			NULL,
+			sw_multi_get_anim,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			(void *)idx,
+			NULL
+		);
+	}
+
+	return idx;
+}
+
+void sw_destroy() {
+	for (int i = 0; i < (sizeof(&all_basic_sw) / sizeof(all_basic_sw[0])); i++) {
+		XPLMUnregisterCommandHandler(all_basic_sw[i].cmd_toggle, sw_basic_cb, 1, (void *)i);
+	}
+
+	for (int i = 0; i < (sizeof(&all_spring_sw) / sizeof(all_spring_sw[0])); i++) {
+		XPLMUnregisterCommandHandler(all_spring_sw[i].cmd_toggle, sw_spring_cb, 1, (void *)i);
+	}
+
+	for (int i = 0; i < (sizeof(&all_multi_sw) / sizeof(all_multi_sw[0])); i++) {
+		XPLMUnregisterCommandHandler(all_multi_sw[i].cmd_toggle_l, sw_multi_l_cb, 1, (void *)i);
+		XPLMUnregisterCommandHandler(all_multi_sw[i].cmd_toggle_r, sw_multi_r_cb, 1, (void *)i);
+	}
+
+	free(all_basic_sw);
+	free(all_spring_sw);
+	free(all_multi_sw);
 }
--- libswitch.h
+++ libswitch.h
@@ -1,17 +1,7 @@
 #ifndef LIBSWITCH_H
 #define LIBSWITCH_H
 
-#include <XPLMDataAccess.h>
-#include <XPLMUtilities.h>
-
-struct {
-	int state;
-	float anim_pos;
-	XPLMDataRef dr_state;
-	XPLMDataRef dr_anim;
-	XPLMCommandRef cmd_toggle;
-	float act_gain;
-} typedef switch_t;
+typedef int switch_t;
 
 /**
  * @brief Initialize a basic, 2 position switch
@@ -21,9 +11,48 @@ struct {
  * @param cmd_name Name of the switch's toggle command. This should be used in clickspots.
  * @param cmd_desc Description to show up in the X-Plane UI.
  *
- * @return A pointer to the switch object.
+ * @return An ID for the switch.
  */
-switch_t *sw_basic_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc);
+switch_t sw_basic_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc);
+
+/**
+ * @brief Initialize a basic, spring-loaded, 2 position switch
+ *
+ * @param dr_name Name of the switches state dataref. This is for systems, and should not be used for animations.
+ * @param dr_anim_name Name of the switch's animation dataref. This is for animations only, and should not be used for animations.
+ * @param cmd_name Name of the switch's toggle command. This should be used in clickspots.
+ * @param cmd_desc Description to show up in the X-Plane UI.
+ *
+ * @return An ID for the switch.
+ */
+switch_t sw_spring_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name, const char *cmd_desc);
+
+/**
+ * @brief Initialize a switch with multiple positions.
+ *
+ * @param dr_name Name of the switches state dataref. This is for systems, and should not be used for animations.
+ * @param dr_anim_name Name of the switch's animation dataref. This is for animations only, and should not be used for animations.
+ * @param cmd_name_l Name of the switch's toggle command. This should be used in clickspots.
+ * @param cmd_desc_l Description to show up in the X-Plane UI.
+ * @param cmd_name_r Name of the switch's toggle command. This should be used in clickspots.
+ * @param cmd_desc_r Description to show up in the X-Plane UI.
+ * @param min_range Minimum value the switch can be.
+ * @param max_range Maximum value the switch can be.
+ * @param default_value Default value of the switch.
+ * @param starter If true, the last position will be spring-loaded.
+ *
+ * @return An ID for the switch.
+ */
+switch_t sw_multi_init(const char *dr_name, const char *dr_anim_name, const char *cmd_name_l, const char *cmd_desc_l, const char *cmd_name_r, const char *cmd_desc_r, int min_range, int max_range, int default_value, bool starter);
+
+int sw_basic_get_state(void *inRefcon);
+void sw_basic_write_state(void *inRefcon, int inValue);
+
+int sw_spring_get_state(void *inRefcon);
+void sw_spring_write_state(void *inRefcon, int inValue);
+
+int sw_multi_get_state(void *inRefcon);
+void sw_multi_write_state(void *inRefcon, int inValue);
 
 /**
  * @brief Refreshes all registered switches.
@@ -33,6 +62,6 @@ void sw_ref();
 /**
  * @brief Destroys all registered switches.
  */
-void sw_destroy_all();
+void sw_destroy();
 
 #endif //LIBSWITCH_H
